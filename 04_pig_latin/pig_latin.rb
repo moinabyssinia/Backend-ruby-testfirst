273bdf778c347b7b4be8d50f8e3a093848c60e59
moinabyssinia <michaelg.tadesse@gmail.com>
2021-11-15 23:31:04 -0500

    finished pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,40 @@
 #write your code here
+
+def translate(word)
+    # define vowels
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    
+    # convert word to array
+    word = word.split()
+
+    # new array to hold new words
+    new_word = []
+
+    word.each do |item|
+        
+        vowel_char_ind = []
+        # isSinglePhoneme = false
+
+        # starts with vowel?
+        if vowel.include? item[0] 
+            item = item + "ay"
+        elsif item[0..1] == "qu"
+            item = item[2..-1] + item[0..1] + "ay"
+        else
+            item.each_char do |char|
+                if (vowel.include? char) and not ((char == "u") and (item[item.index(char) - 1] = "q"))
+                    vowel_char_ind.push(item.index(char))
+
+                end
+            end
+            
+            item = item[vowel_char_ind[0]..-1] + item[0..vowel_char_ind[0] - 1] + "ay"
+
+        end
+
+        new_word.push(item)
+
+    end
+
+   new_word.join(" ")
+end

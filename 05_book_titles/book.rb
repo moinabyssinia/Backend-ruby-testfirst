c98ddc71d355281b23bcc49f8bd852cff42bc9c4
moinabyssinia <michaelg.tadesse@gmail.com>
2021-11-16 00:13:03 -0500

    finished book class

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,36 @@
 class Book
-# write your code here
+    # write your code here
+    attr_accessor :title
+    def initialize
+        @title = ""
+    end
+
+    def title
+        title_array = @title.split()
+        exceptions = ['the', 'and', 'a', 'an', 'in', 'of']
+        title_cap = []
+        count = 0 
+
+        title_array.each do |item|
+            unless exceptions.include? item
+                title_cap.push(item.capitalize)
+            else
+                if count == 0
+                    title_cap.push(item.capitalize)
+                else
+                    title_cap.push(item)
+                end
+            end
+
+            count +=1
+            
+        end
+        title_cap.join(" ")
+    end
+
 end
+
+
+# book = Book.new 
+# puts book.title = "inferno"
+# puts book.title
